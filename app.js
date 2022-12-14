ae3504f387aa0696f9ff19e40769e25b9b57286b
SHYAM KHADE <khade.shyam10.1@gmail.com>
2022-12-14 18:43:52 +0530

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const _ = require('lodash');
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
@@ -44,27 +43,12 @@ const item3 = new Item({
 });
 
 
-const defaultItems = [item1, item2, item3];
-
-
 
 app.get("/", (req, res) => {
 
     Item.find({}, (err, foundItems) => {
-        if (foundItems.length === 0) {
-            Item.insertMany(defaultItems, function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Successfully saved to itemsDB");
-                }
-            });
-            res.redirect("/");
-        }
-        else{
           res.render("list",{listTitle:"Today",items:foundItems});
-        }
-
+    
     })
 });
 
@@ -79,7 +63,7 @@ app.get('/:customListName', (req, res) => {
                 console.log("list not found");
                 const list = new List({
                     name:customListname,
-                    items:defaultItems
+                    items:[]
                  });
                  list.save();
                res.redirect("/"+customListname);
@@ -137,8 +121,7 @@ app.post("/delete",(req,res)=>{
          }
       })
     }
-    
-   
+  
 })
 
 app.listen(3000, () => {
